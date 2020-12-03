73f54f5821f9704f06ac1b5137076b08e69dcaf1
geNia <geniann93@gmail.com>
2020-12-03 16:46:57 -0500

    Added an ACS special to check whether the game is in demo or not
    (cherry picked from commit 35cdcae676aaf981008f9acb6f358256ac3faeb0)

--- src/p_acs.cpp
+++ src/p_acs.cpp
@@ -4972,6 +4972,7 @@ enum EACSFunctions
 	ACSF_Strftime,
 	ACSF_SetDeadSpectator,
 	ACSF_SetActivatorToPlayer,
+	ACSF_InDemoMode,
 
 	// ZDaemon
 	ACSF_GetTeamScore = 19620,	// (int team)
@@ -6856,6 +6857,9 @@ doplaysound:			if (funcIndex == ACSF_PlayActorSound)
 				return 1;
 			}
 			break;
+
+		case ACSF_InDemoMode:
+			return CLIENTDEMO_IsPlaying() ? 1 : 0;
 
 		case ACSF_GetActorFloorTexture:
 		{
